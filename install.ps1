ba808d7a06beb35de39c8155d0ebb914e4180202
howardcc <howard.chingchung@radboudumc.nl>
2024-06-27 17:28:15 +0200

    added README.md for chocolatey and winget

--- install.ps1
+++ install.ps1
@@ -1,46 +1,13 @@
+﻿write-host 'install winget package manager from ms store'
+write-host 'https://github.com/microsoft/winget-cli'
+write-host 'https://www.microsoft.com/p/app-installer/9nblggh4nns1'
+write-host ''
+write-host 'install chocolatey package manager'
+write-host 'has to be run or copied directly into terminal administrative mode'
+write-host ''
+write-host 'Set-ExecutionPolicy Unrestricted -Force (or [A]ccept)'
+write-host 'the run: Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))'
 
-choco install powershell-core
-#or windows-terminal
-choco install alacritty
-choco install fzf ripgrep bat
-choco install mingw 
+# https://ravpn.radboudumc.nl
 
-#download fonts
-winget install JanDeDobbeleer.OhMyPosh
-
-Install-Module z -AllowClobber
-Install-Module -Name Terminal-Icons
-
-$docs = $([Environment]::GetFolderPath("mydocuments"))
-$prof = Join-Path -Path $docs -ChildPath 'Powershell'
-
-cp Profile.ps1 $prof
-cp ohmyposhv3-v2.json $prof
-
-#personal
-git config --global user.email "howardchingchung@gmail.com"
-git config --global user.name "howardcc"
-
-#radboud
-#git config --global user.email "howard.chingchung@radboudumc.com"
-#git config --global user.name "howardcc"
-
-
-# PSReadLine
-# see github for instructions
-# you may need to install/update it from cmd.exe because it's in use 
-# powershell -noprofile -command "Install-Module PSReadLine -AllowPrerelease"
-# otherwise try: Install-Module PSReadLine -AllowPrerelease -Force
-#curl wget https://github.com/PowerShell/PSReadLine/archive/refs/tags/v2.2.5.zip -O
-
-#need 2.2.5
-#$(get-module PSReadLine).Version
-#Remove-Module PSReadLine
-
-
-#copy Profile.ps1 to users\documents\powershell dir C:\Users\Howie\Documents\PowerShell and change settings
-# Profle.ps1 contains psreadline functions that are loaded everytime powershell is started.
-
-#winget install ZoomIt
-#curl https://live.sysinternals.com/ZoomIt.exe -O
-#Invoke-WebRequest https://live.sysinternals.com/ZoomIt.exe -PassThru -Outfile Zoomit.Exe
+choco feature enable -n allowGlobalConfirmation
